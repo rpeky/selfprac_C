7005f56e656da7388c0591684b7a40e414fcbff5
peky <ryan_pek@mymail.sutd.edu.sg>
2024-10-24 03:03:00 +0800

    finished int que, to follow up on super stack and q

--- datastrucs/queue/intqueue.c
+++ datastrucs/queue/intqueue.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 typedef struct intstack{
 	int *stack;
@@ -7,7 +8,7 @@ typedef struct intstack{
 	unsigned maxstack;
 } intstack;
 
-void initstack(mystack *a, unsigned initialsz){
+void initstack(intstack *a, unsigned initialsz){
 	a->stack= (int*)malloc(initialsz*sizeof(int));
 	if (a->stack==NULL){
 		printf("malloc failed\n");
@@ -17,7 +18,7 @@ void initstack(mystack *a, unsigned initialsz){
 	a->maxstack=initialsz;
 }
 
-void push(mystack *a, int x){
+void push(intstack *a, int x){
 	//check for overflows if pushing to stack
 	if(a->maxstack > UINT_MAX/2){
 		printf("stack size overflow\n");
@@ -36,7 +37,7 @@ void push(mystack *a, int x){
 	a->size+=1;
 }
 
-int pop(mystack *a){
+int pop(intstack *a){
 	if(a->size==0){
 		printf("empty stack, unable to pop\n");
 		return -1;
@@ -45,27 +46,76 @@ int pop(mystack *a){
 	return a->stack[a->size];
 }
 
+int peek(intstack *a) {
+    if (a->size == 0) {
+        printf("Empty stack\n");
+        return -1;
+    }
+    return a->stack[a->size - 1];
+}
+
+
 typedef struct intqueue{
 	intstack left_stack;
 	intstack right_stack;
 } intqueue;
 
 void initqueue(intqueue *a){
-	initstack(a->&left_stack, 2);
-	initstack(a->&right_stack, 2);
+	initstack(&a->left_stack, 2);
+	initstack(&a->right_stack, 2);
 }
 
 void enqueue(intqueue *a, int x){
-	a->left_stack.push(x);
+	push(&a->left_stack,x);
 }
 
 int dequeue(intqueue *a){
-	if(a->right_stack->size > 0){
-		return ;
+	//flush left to right stack if there is nothing in right
+	if(a->right_stack.size == 0){
+		while(a->left_stack.size>0)
+			push(&a->right_stack, pop(&a->left_stack));
 	}
+
+	//right stack is still empty
+	if(a->right_stack.size==0){
+		printf("Queue empty\n");
+		return -1;
+	}
+
+	//return dequeued item
+	return pop(&a->right_stack);
+
+}
+
+int peek_queue(intqueue *a){
+	if (a->right_stack.size == 0){
+		while (a->left_stack.size > 0) {
+			push(&a->right_stack, pop(&a->left_stack));
+        }
+    }
+
+    if (a->right_stack.size == 0) {
+        printf("Empty queue\n");
+        return -1;
+    }
+
+    return peek(&a->right_stack);  // Use the stack peek function
 }
 
 int main(int argc, char** argv){
-	printf("test\n");
+	intqueue newq;
+	initqueue(&newq);
+	enqueue(&newq, 1);
+	enqueue(&newq, 2);
+	enqueue(&newq, 3);
+
+	printf("Peeking1: %d\n",peek_queue(&newq));
+	printf("Dequeue1: %d\n",dequeue(&newq));
+	printf("Peeking2: %d\n",peek_queue(&newq));
+	printf("Dequeue2: %d\n",dequeue(&newq));
+	printf("Peeking3: %d\n",peek_queue(&newq));
+	printf("Dequeue3: %d\n",dequeue(&newq));
+	printf("Dequeue-1: %d\n",dequeue(&newq));
+
 	return 0;
 }
